afb6c46718d03017d847490e118481659ca59094
Ferran_Gandia <fgandia@edu.tecnocampus.cat>
2022-09-28 12:27:28 +0200

    Initial commit

---
+++ src/main/java/com/example/lab_prac1/LabPrac1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab_prac1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPrac1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPrac1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab_prac1/LabPrac1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPrac1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
